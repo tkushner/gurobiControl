10cb4b0e4c935d72b01b7daf95b7bf6097ccabaf
Taisa Kushner <taisa.kushner@colorado.edu>
2017-08-03 15:37:29 -0600

    Updated code for plotting (colors, labels, etc), and new cluster runs

--- makeplots.m
+++ makeplots.m
@@ -22,10 +22,10 @@ for i=1:MAX
     figure(i)
     hold on;
     for n = 1 : numel(Data(i).upbnd)
-        plot([Data(i).times(n),Data(i).times(n)], [Data(i).upbnd(n) Data(i).lowbnd(n)],'linewidth',10);
+        plot([Data(i).times(n),Data(i).times(n)], [Data(i).upbnd(n) Data(i).lowbnd(n)],'Color',[.612,.769,.635],'linewidth',10);
     end
-    title(strcat({'Reachability analysis for: '},strrep(Data(i).lbl,'.csv','')))
-    xlabel('depth (time)')
-    ylabel('glucose concentration (mg/dL)')
+    title(strcat({'Reachability analysis for: '},strrep(strrep(Data(3).lbl, '.csv',''),'_',{' '})),'FontSize',16)
+    xlabel('depth (time)','FontSize',14)
+    ylabel('glucose concentration (mg/dL)','FontSize',14)
     hold off;
 end
